1079610b5d49b9abbbb20accf027884502813c9f
Shash <121744832+ShashTheEpic@users.noreply.github.com>
2023-05-20 20:41:31 +1000

    Updated

--- Batch Installer/RCO.bat
+++ Batch Installer/RCO.bat
@@ -1,16 +1,16 @@
 @echo off
 
-REM Obtains latest version of ClientAppSettings.json 
+REM Obtains the latest version of ClientAppSettings.json 
 set RCO_url=https://raw.githubusercontent.com/L8X/Roblox-Client-Optimizer/main/assets/ClientAppSettings.json 
 REM https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json is an alternative link to use
 set RobloxVersionDir=C:\Users\%username%\AppData\Local\Roblox\Versions
 
-REM Writes ClientAppSettings.json to RCO.json in temporary folder
+REM Writes ClientAppSettings.json to RCO.json in the temporary folder
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%RCO_url%', 'RCO.json')"
 
 REM Finds the latest Roblox folder
 for /f "delims=" %%I in ('dir /b /ad /o-d "%RobloxVersionDir%\version-*"') do (
-    set LatestRobloxVersionDir=%RobloxVersionDir%\%%I
+    set "LatestRobloxVersionDir=%RobloxVersionDir%\%%I"
     goto :WriteRCO
 )
 
@@ -23,9 +23,21 @@ if not exist "%LatestRobloxVersionDir%\ClientSettings" (
 REM RCO.json is copied to the ClientSettings folder
 copy /y "RCO.json" "%LatestRobloxVersionDir%\ClientSettings\ClientAppSettings.json" >nul
 
-REM RCO.json which was downloaded to the temporary folder is deleted
+REM RCO.json, which was downloaded to the temporary folder, is deleted
 del "RCO.json"
 
-REM Informs user that RCO has been updated/installed and the directory
+REM Informs the user that RCO has been updated/installed and displays the directory
 echo New data has been written to %LatestRobloxVersionDir%\ClientSettings\ClientAppSettings.json
+
+REM Asks the user if they want the updater added to startup
+set /p Startup?="Do you want to add this script to startup? (Y/N): "
+if /i "%Startup?%"=="Y" (
+    REM Adds the script to startup
+    echo Set objShell = CreateObject("WScript.Shell"^) > "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RCO_Update.vbs"
+    echo objShell.Run """%~dpnx0""", 0, False >> "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RCO_Update.vbs"
+    echo WScript.Quit >> "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RCO_Update.vbs"
+    echo Added to startup.
+)
+
+REM Paused so the command prompt window doesn't close
 pause
